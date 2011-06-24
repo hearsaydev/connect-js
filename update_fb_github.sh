79828c389d75b62c3b38db808b4afd4e3fff8aae
Roger Hu <rhu@hearsaycorp.com>
2011-06-23 18:37:29 -0700

    updated script

--- update_fb_github.sh
+++ update_fb_github.sh
@@ -1,6 +1,16 @@
+#!/bin/bash -ex
 cd `dirname $0`
 /usr/bin/wget https://connect.facebook.net/en_US/all.js -O all.js
 /usr/bin/python jsbeautifier.py -o all_deminified.js all.js
-/usr/bin/git add -u
-/usr/bin/git commit -m `date --rfc-3339=date`
-/usr/bin/git push origin master
+# Avoid sending out unnecessary updates if only the timestamp has changed.
+ALL_JS_DIFF=`git diff --shortstat all.js | grep -v "2 insertions"`
+#ALL_JS_DIFF=`git diff .`
+if [ ! -z "$ALL_JS_DIFF" ]                            
+then                                                  
+  echo "Commit has changed..."                        
+  git --no-pager diff .
+  /usr/bin/git add -u
+  /usr/bin/git commit -m `date --rfc-3339=date`       
+  /usr/bin/git push -f origin master                      
+fi                                                    
+                                                      
