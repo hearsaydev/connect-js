fce0a97077f3d0f7f882715b63f8ce44e8a70cc1
Roger Hu <rhu@hearsaycorp.com>
2012-05-23 10:00:02 -0700

    Facebook Connect changes for 2012-05-23

--- all_deminified.js
+++ all_deminified.js
@@ -1,4 +1,4 @@
-/*1337729823,169939314,JIT Construction: v561754,en_US*/
+/*1337790462,169914741,JIT Construction: v561754,en_US*/
 
 var FB;
 if (!FB) {
@@ -7161,7 +7161,7 @@ FB.widgetPipeEnabledApps = {
     "179150165472010": 1
 };
 FB.widgetPipeTagCountThreshold = 4;
-FB._iframeLoginButton = false;
+FB._iframeLoginButton = true;
 FB.provide("TemplateData", {
     "_enabled": 0
 }, true);
